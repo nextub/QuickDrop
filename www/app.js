cc18176a6adc697fb7649a2f52cd96b3713bdcb3
Afshin Moazami <afshin.moazami@guavus.com>
2015-11-14 22:05:02 -0500

    fix

--- www/app.js
+++ www/app.js
@@ -747,7 +747,7 @@ var NG_MODULE = 'MyApp'
 		self.pop = pop;
 		self.pops = pops;
 		self.stack = stack;
-		self.defaultState = 'map';
+		self.defaultState = 'login';
 		self.states = {
 			'login': {
 				templateUrl: 'templates/login.html'
