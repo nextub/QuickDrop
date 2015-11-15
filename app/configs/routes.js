f12d9d2fe8c560c34caa3d5b5f59bb2f2761a11d
Afshin Moazami <afshin.moazami@guavus.com>
2015-11-14 19:31:08 -0500

    markers

--- app/configs/routes.js
+++ app/configs/routes.js
@@ -8,7 +8,7 @@
 		self.pop = pop;
 		self.pops = pops;
 		self.stack = stack;
-		self.defaultState = 'categories';
+		self.defaultState = 'map';
 		self.states = {
 			'login': {
 				templateUrl: 'templates/login.html'
